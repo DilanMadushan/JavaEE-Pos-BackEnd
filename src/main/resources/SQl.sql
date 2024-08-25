4a5fdf09a9e5abb908fb2400795bf3cecaad189d
Dilan Fernando <dilanmadushanka6013@gmail.com>
2024-08-25 18:03:32 +0530

    database is done

--- src/main/resources/SQl.sql
+++ src/main/resources/SQl.sql
@@ -1,8 +1,8 @@
 create table customer(
     id varchar(100)primary key,
-    name varchar(100),
-    address varchar(100),
-    tel varchar(10)
+    name varchar(100)not null,
+    address varchar(100)not null,
+    tel varchar(10)not null
 );
 
 create table product(
@@ -10,4 +10,23 @@ create table product(
     name varchar(100)not null ,
     price double not null,
     qty double not null
+);
+
+create table orders(
+    id varchar(100)primary key,
+    cusId varchar(100)not null,
+    date date not null,
+    foreign key(cusId)references customer(id)
+        on update cascade on delete cascade
+);
+
+create table orderDetails(
+    orderId varchar(100)not null ,
+    proId varchar(100)not null ,
+    qty double not null ,
+    price double not null ,
+    foreign key(orderId)references orders(id)
+        on update cascade on delete cascade,
+    foreign key(proId)references product(id)
+        on update cascade on delete cascade
 );
